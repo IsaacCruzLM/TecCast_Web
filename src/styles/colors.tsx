b73b112a20aaac109cf0fb3d3595903ca163b7c5
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-06 14:16:20 -0300

    Configura ambiente do projeto

---
+++ src/styles/colors.tsx
@@ -0,0 +1,11 @@
+const colors = {
+  primary0: '#14be24',
+  primary1: '#138d20',
+  primary2: '#052d12',
+  light: '#FFFFFF',
+  dark: '#100708,',
+  danger: '#E56B5F',
+  success: '#62D793',
+};
+
+export default colors;
